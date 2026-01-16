d5d8064500ab61cbf054151b85f986cf957b675c
Pjdur <pjdurable@gmail.com>
2026-01-16 20:12:41 +0000

    feat: add Cliux input for styling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use cliux::Input;
 use gh_models::{GHModels, types::ChatMessage};
 use std::env;
-use std::io::{self, Write};
 use clap::Parser;
 use colorline::Style;
+use crossterm::{execute, terminal::{Clear, ClearType}, cursor::MoveTo};
+use std::io::stdout;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -26,11 +28,27 @@ async fn main() {
     ];
 
     loop {
-        print!("{}", "You: ".green());
-        io::stdout().flush().unwrap();
+        // I don't know why but I have to use both All and Purge
+        // to clear the visible screen and scrollback
+        execute!(stdout(), Clear(ClearType::All)).unwrap();
+        execute!(stdout(), Clear(ClearType::Purge)).unwrap();
+        execute!(stdout(), MoveTo(0,0)).unwrap();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        for msg in &messages {
+            match msg.role.as_str() {
+                "user" => println!("{} {}", "You:".green(), msg.content),
+                "assistant" => println!("{} {}", "Assistant:".blue(), msg.content),
+                _ => {}
+            }
+        }
+
+        let input = Input::new("Enter your message")
+            .bold(true)
+            .color("cyan")
+            .style("rounded")
+            .width(50)
+            .prompt();
+
         let input = input.trim().to_string();
 
         if input == "exit" {
@@ -48,7 +66,6 @@ async fn main() {
             .unwrap();
 
         let reply = response.choices[0].message.content.clone();
-        println!("{}: {}", "Assistant".blue(), reply);
 
         messages.push(ChatMessage {
             role: "assistant".into(),
